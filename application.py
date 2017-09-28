a7a481a00ecd9f6fd021b0ff6b07df0bb3bd4aed
Riley Brazell <rileybrazell@gmail.com>
2017-09-28 09:08:15 -0700

    Add, delete, and edit categories to database

--- application.py
+++ application.py
@@ -20,6 +20,50 @@ def showCategories():
 	return render_template('categories.html', categories=categories)
 
 
+## Create new category ##
+# Adds new entry to Category table
+@app.route('/category/new/', methods=['GET', 'POST'])
+def newCategory():
+	if request.method == 'POST':
+		newCategory = Category(name=request.form['name'])
+		session.add(newCategory)
+		session.commit()
+		return redirect(url_for('showCategories'))
+	else:
+		return render_template('newCategory.html')
+
+
+## Edit existing category name ##
+# URLs contain category id, used to get entry from Category table and commit changes to name
+@app.route('/category/<int:category_id>/edit/', methods=['GET', 'POST'])
+def editCategory(category_id):
+	editedCategory = session.query(Category).filter_by(id=category_id).one()
+
+	if request.method == 'POST':
+		if request.form['name']:
+			editedCategory.name = request.form['name']
+			session.add(editedCategory)
+			session.commit()
+			return redirect(url_for('showCategories'))
+	else:
+		return render_template('editCategory.html', category=editedCategory)
+
+
+## Delete existing category ##
+# Get entry from Category table, delete, and commit deletion
+@app.route('/category/<int:category_id>/delete/', methods=['GET', 'POST'])
+def deleteCategory(category_id):
+	categoryToDelete = session.query(Category).filter_by(id=category_id).one()
+
+	if request.method == 'POST':
+		if request.form['confirm']:
+			session.delete(categoryToDelete)
+			session.commit()
+			return redirect(url_for('showCategories'))
+	else:
+		return render_template('deleteCategory.html', category=categoryToDelete)
+
+
 ## Clicking on a category name will show a list of items in that category ##
 # Each item is associated with a category id on creation, used to build this list
 @app.route('/category/<int:category_id>/')
