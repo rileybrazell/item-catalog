16f9493b4ab9adfed354942df655752502dc3a87
Riley Brazell <rileybrazell@gmail.com>
2017-09-28 13:58:58 -0700

    Add login page with button

--- application.py
+++ application.py
@@ -28,8 +28,8 @@ def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
-    return "The current session state is %s" % login_session['state']
-    # return render_template('login.html', STATE=state)
+    # return "The current session state is %s" % login_session['state']
+    return render_template('login.html', STATE=state)
 
 
 ## JSON endpoints for db items ##
