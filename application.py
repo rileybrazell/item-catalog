cb6cf6d50e6beed2b349833a815ed2949a5f1348
Riley Brazell <rileybrazell@gmail.com>
2017-09-30 19:18:12 -0700

    OAuth2 Google disconnect/logout

--- application.py
+++ application.py
@@ -107,6 +107,37 @@ def gconnect():
 	flash("you are now logged in as %s" % login_session['username'])
 	return output
 ## GCONNECT END ##
+
+
+## GDISCONNECT START ##
+@app.route('/gdisconnect')
+def gdisconnect():
+	# Check if user is connected, only disconnect a connected user
+	credentials = login_session.get('credentials')
+	if credentials is None:
+		response = make_response(json.dumps('Current user not connected.'), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+	# Use HTTP GET to revoke current token with Google
+	url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % credentials
+	h = httplib2.Http()
+	result = h.request(url, 'GET')[0]
+
+	if result['status'] == '200':
+		# Reset the user's session
+		del login_session['credentials']
+		del login_session['gplus_id']
+		del login_session['username']
+		del login_session['email']
+
+		response = make_response(json.dumps('Successfully disconnected.'), 200)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+	else:
+		# If the token was invalid for any reason
+		response = make_response(json.dumps('Failed to revoke token for given user'), 400)
+		response.headers['Content-Type'] = 'application/json'
+		return response
 ### OAuth login code end ###
 
 
